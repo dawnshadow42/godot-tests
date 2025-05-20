e4812f0a6096dcf173f281cb821af5bb7d43af7f
dawnshadow42 <ljsieber@gmail.com>
2025-05-20 10:10:16 -0500

    added audio
    added audio and tree hit counter for collision
    added audio for physics

--- 4minis/collision/playermovement.gd
+++ 4minis/collision/playermovement.gd
@@ -1,13 +1,30 @@
 extends RigidBody3D
 @export var move_speed : float = 2.0
+@export var tree_count : int = 0
+var score_label
+
+func _ready():
+	score_label = get_parent().get_node("Label")
+	tree_count = 0
 
 func _physics_process(delta):
 	if Input.is_key_pressed(KEY_LEFT):
 		linear_velocity.x = -move_speed
 	if Input.is_key_pressed(KEY_RIGHT):
 		linear_velocity.x = move_speed
+# Update score and display
+	score_label.text = "Trees hit: " + str(tree_count)
 
 func _on_body_entered(body):
 	if body.is_in_group("tree"):
-		get_tree().reload_current_scene()
-		
+		tree_count += 1
+		print(tree_count)
+		if tree_count < 9:
+			$sbmin.play()
+		else:
+			$hsoops.play() 
+	elif body.is_in_group("deathplane"):
+		$hsoops.play()
+
+func _on_hsoops_finished():
+	get_tree().reload_current_scene()
--- 4minis/physics/physicsplayer.gd
+++ 4minis/physics/physicsplayer.gd
@@ -7,3 +7,6 @@ func _process(_delta):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		var dir=global_position.direction_to(get_global_mouse_position())
 		apply_impulse(dir*hit_force)
+		$sbmax.play()
+func _on_body_entered(body):
+	$sbmin.play()
